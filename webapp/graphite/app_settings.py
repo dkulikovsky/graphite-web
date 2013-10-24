824dc8823b8500845499339cde1131e9e6f746db
Dmitry Kulikovsky <dkulikovsky@yandex-team.ru>
2013-10-24 17:07:48 +0400

    fixed path to files, admin/media is too long

--- webapp/graphite/app_settings.py
+++ webapp/graphite/app_settings.py
@@ -46,7 +46,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin/media/'
 
 # List of callables that know how to import templates from various sources.
 #XXX Compatibility for Django 1.1. To be removed after 0.9.10
